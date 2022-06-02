b5fcf2f890f5ac3700cc6bf9b8c99500061a0883
Kerim Dinçer <kerim_dincer@hotmail.com>
2022-06-02 19:29:04 +0300

    AutoMapper, Service Layer, DTOs

--- NLayer.Repository/Seeds/CategorySeed.cs
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using NLayer.Core;
+using NLayer.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NLayer.Repository.Seeds 
+namespace NLayer.Repository.Seeds
 {
     internal class CategorySeed : IEntityTypeConfiguration<Category>
     {
